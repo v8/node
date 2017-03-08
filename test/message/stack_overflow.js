64943a46f1381c0fbda20ed0745526a9ca6837ec
Yang Guo <yangguo@chromium.org>
2017-03-08 12:59:24 +0100

    Fix test expectation.

--- test/message/stack_overflow.js
+++ test/message/stack_overflow.js
@@ -6,8 +6,8 @@ Error.stackTraceLimit = 0;
 console.error('before');
 
 // Trigger stack overflow by stringifying a deeply nested array.
-var array = [];
-for (var i = 0; i < 100000; i++) {
+let array = [];
+for (let i = 0; i < 100000; i++) {
   array = [ array ];
 }
 
