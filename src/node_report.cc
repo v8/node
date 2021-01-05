6aa588b38a553700241858b820cf97dd6d35fefd
Victor Gomes <victorgomes@chromium.org>
2021-01-05 11:18:38 +0100

    [fuchsia] Fuchsia support to Node
    # Conflicts:
    #	src/node_process_methods.cc
    #	src/node_report.cc

--- src/node_report.cc
+++ src/node_report.cc
@@ -13,7 +13,9 @@
 #include <Windows.h>
 #else  // !_WIN32
 #include <cxxabi.h>
+#ifndef __Fuchsia__
 #include <sys/resource.h>
+#endif
 #include <dlfcn.h>
 #endif
 
@@ -642,6 +644,7 @@ static void PrintResourceUsage(JSONWriter* writer) {
       (uv_hrtime() - node::per_process::node_start_time) / (NANOS_PER_SEC);
   if (uptime == 0) uptime = 1;  // avoid division by zero.
 
+#ifndef __Fuchsia__
   // Process and current thread usage statistics
   uv_rusage_t rusage;
   writer->json_objectstart("resourceUsage");
@@ -666,6 +669,7 @@ static void PrintResourceUsage(JSONWriter* writer) {
     writer->json_objectend();
   }
   writer->json_objectend();
+#endif
 #ifdef RUSAGE_THREAD
   struct rusage stats;
   if (getrusage(RUSAGE_THREAD, &stats) == 0) {
@@ -710,7 +714,7 @@ static void PrintSystemInformation(JSONWriter* writer) {
 
   writer->json_objectend();
 
-#ifndef _WIN32
+#if !defined(_WIN32) && !defined(__Fuchsia__)
   static struct {
     const char* description;
     int id;
@@ -757,7 +761,7 @@ static void PrintSystemInformation(JSONWriter* writer) {
     }
   }
   writer->json_objectend();
-#endif  // _WIN32
+#endif  // _WIN32 && __Fuchsia__
 
   PrintLoadedLibraries(writer);
 }
