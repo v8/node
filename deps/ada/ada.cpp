edf28f98167377a55e6c69617e3228ed6a2e245a
pthier <pthier@chromium.org>
2023-03-17 11:17:55 +0100

    Fix ada OOB read

--- deps/ada/ada.cpp
+++ deps/ada/ada.cpp
@@ -2241,7 +2241,7 @@ namespace ada::parser {
           ada_log("NO_SCHEME ", helpers::substring(url_data, input_position));
           // If base is null, or base has an opaque path and c is not U+0023 (#), validation error, return failure.
           // SCHEME state updates the state to NO_SCHEME and validates url_data is not empty.
-          if (base_url == nullptr || (base_url->has_opaque_path && url_data[input_position] != '#')) {
+          if (base_url == nullptr || (base_url->has_opaque_path && (input_position != input_size || !url.fragment.has_value()))) {
             ada_log("NO_SCHEME validation error");
             url.is_valid = false;
             return url;
