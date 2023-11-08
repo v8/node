61d7098c4bad049ad5a558bff7f4ea8aa999726e
Victor Gomes <victorgomes@chromium.org>
2023-11-08 12:15:30 +0100

    Adapt tests for increased TypedArray sizes (#161)
    V8 will increase the maximum TypedArray size in
    https://crrev.com/c/4872536; this CL adapts the tests to allow for that.
    
    Tests that hard-code smaller sizes or are already tested in V8 are
    removed; one test is adapted to not rely on a specific limit.
    # Conflicts:
    #	test/parallel/test-buffer-alloc.js
    #	test/parallel/test-buffer-tostring-rangeerror.js

--- test/parallel/test-buffer-tostring-rangeerror.js
+++ test/parallel/test-buffer-tostring-rangeerror.js
@@ -13,13 +13,23 @@ const {
   },
 } = require('buffer');
 
-const len = MAX_STRING_LENGTH + 1;
+// Find the maximum supported buffer length.
+let limit = 1 << 31; // 2GB
+while (true) {
+  try {
+    Buffer(limit);
+    limit *= 2;
+  } catch (e) {
+    break;
+  }
+}
+
 const message = {
   code: 'ERR_STRING_TOO_LONG',
   name: 'Error',
 };
-assert.throws(() => Buffer(len).toString('utf8'), message);
-assert.throws(() => SlowBuffer(len).toString('utf8'), message);
-assert.throws(() => Buffer.alloc(len).toString('utf8'), message);
-assert.throws(() => Buffer.allocUnsafe(len).toString('utf8'), message);
-assert.throws(() => Buffer.allocUnsafeSlow(len).toString('utf8'), message);
+assert.throws(() => Buffer(limit).toString('utf8'), message);
+assert.throws(() => SlowBuffer(limit).toString('utf8'), message);
+assert.throws(() => Buffer.alloc(limit).toString('utf8'), message);
+assert.throws(() => Buffer.allocUnsafe(limit).toString('utf8'), message);
+assert.throws(() => Buffer.allocUnsafeSlow(limit).toString('utf8'), message);
