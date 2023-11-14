4939bc855af3b3e4f5a29bae3167df2b9fd2ce40
Clemens Backes <post@clemens-backes.de>
2023-11-14 15:21:49 +0100

    Avoid deprecated TypedArray::kMaxLength field (#170)
    Use TypedArray::kMaxByteLength instead.

--- src/node_buffer.h
+++ src/node_buffer.h
@@ -29,7 +29,7 @@ namespace node {
 
 namespace Buffer {
 
-static const size_t kMaxLength = v8::Uint8Array::kMaxLength;
+static const size_t kMaxLength = v8::TypedArray::kMaxByteLength;
 
 typedef void (*FreeCallback)(char* data, void* hint);
 
