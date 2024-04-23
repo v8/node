bfa03fa86afc21b2eee9b111306c3ed39bbb55ca
Victor Gomes <victorgomes@chromium.org>
2024-04-23 10:17:55 +0200

    Add missing trycatch (#171)
    # Conflicts:
    #	src/js_stream.cc

--- src/js_stream.cc
+++ src/js_stream.cc
@@ -163,6 +163,7 @@ void JSStream::Finish(const FunctionCallbackInfo<Value>& args) {
 
 
 void JSStream::ReadBuffer(const FunctionCallbackInfo<Value>& args) {
+  v8::TryCatch try_catch(args.GetIsolate());
   JSStream* wrap;
   ASSIGN_OR_RETURN_UNWRAP(&wrap, args.This());
 
