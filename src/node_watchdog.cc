0a668669ed77f5472b7efabc705ec8cd6ba77718
Victor Gomes <victorgomes@chromium.org>
2024-11-18 15:09:14 +0100

    [fuchsia] Fuchsia support to Node

--- src/node_watchdog.cc
+++ src/node_watchdog.cc
@@ -298,6 +298,7 @@ int SigintWatchdogHelper::Start() {
     return 0;
   }
 
+#ifndef __Fuchsia__
 #ifdef __POSIX__
   CHECK_EQ(has_running_thread_, false);
   has_pending_signal_ = false;
@@ -326,6 +327,7 @@ int SigintWatchdogHelper::Start() {
     SetConsoleCtrlHandler(WinCtrlCHandlerRoutine, TRUE);
   }
 #endif
+#endif
 
   return 0;
 }
@@ -353,6 +355,7 @@ bool SigintWatchdogHelper::Stop() {
     watchdogs_.clear();
   }
 
+#ifndef __Fuchsia__
 #ifdef __POSIX__
   if (!has_running_thread_) {
     has_pending_signal_ = false;
@@ -373,7 +376,7 @@ bool SigintWatchdogHelper::Stop() {
 
   had_pending_signal = has_pending_signal_;
   has_pending_signal_ = false;
-
+#endif
   return had_pending_signal;
 }
 
