6ec1924a2862a56e1bf4d6d729e6c4e961b6f2c8
Victor Gomes <victorgomes@chromium.org>
2024-11-18 15:09:14 +0100

    [fuchsia] Fuchsia support to Node
    # Conflicts:
    #	src/node_report.cc

--- src/node_os.cc
+++ src/node_os.cc
@@ -77,7 +77,15 @@ static void GetHostname(const FunctionCallbackInfo<Value>& args) {
 static void GetOSInformation(const FunctionCallbackInfo<Value>& args) {
   Environment* env = Environment::GetCurrent(args);
   uv_utsname_t info;
-  int err = uv_os_uname(&info);
+  int err = 0;
+#ifdef __Fuchsia__
+  // TODO(victor): Update uv_os_uname to get these informations for Fuchsia
+  info.sysname[0] = 0;
+  info.version[0] = 0;
+  info.release[0] = 0;
+#else
+  err = uv_os_uname(&info);
+#endif
 
   if (err != 0) {
     CHECK_GE(args.Length(), 1);
@@ -349,6 +357,7 @@ static void GetUserInfo(const FunctionCallbackInfo<Value>& args) {
                                         kRetLength));
 }
 
+#ifndef __Fuchsia__
 
 static void SetPriority(const FunctionCallbackInfo<Value>& args) {
   Environment* env = Environment::GetCurrent(args);
@@ -393,6 +402,8 @@ static void GetAvailableParallelism(const FunctionCallbackInfo<Value>& args) {
   unsigned int parallelism = uv_available_parallelism();
   args.GetReturnValue().Set(parallelism);
 }
+
+#endif  // !__Fuchsia__
 
 void Initialize(Local<Object> target,
                 Local<Value> unused,
