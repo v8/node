c712555cabb153c6b60dcadb8c08ec1632628dd4
Victor Gomes <victorgomes@chromium.org>
2026-02-24 14:23:18 +0100

    Revert "lib: throw from localStorage getter on missing storage path"
    This reverts commit 2fb3c7eef595a7315aabf925ba3c1302566391d8.
    See discussion https://github.com/nodejs/node/issues/60704
    The workarounds of passing additional flags or setting envirionment
    variables isn't easily possible on our node-ci environment.

--- test/common/index.js
+++ test/common/index.js
@@ -416,11 +416,6 @@ if (process.env.NODE_TEST_KNOWN_GLOBALS !== '0') {
       if (val === 'crypto' && !hasCrypto) {
         continue;
       }
-      // globalThis.localStorage is a getter that throws if Node.js was
-      // executed without a --localstorage-file path.
-      if (val === 'localStorage' && !hasLocalStorage) {
-        continue;
-      }
       if (!knownGlobals.has(globalThis[val])) {
         leaked.push(val);
       }
